f4e11eeda16ae7d7bace9fef08eb26bc51afe386
shubham shah <shubhamshekharshah@gmail.com>
2020-02-23 03:27:26 +0530

    Challenge 3

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -22,6 +22,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .play, target: self, action: #selector(showScoreTapped))
+        
         countries += ["estonia","france","germany","ireland","italy","monaco","nigeria","poland","russia","spain","uk","us"]
         
         button1.layer.borderWidth = 1
@@ -83,6 +85,12 @@ class ViewController: UIViewController {
             ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
             present(ac,animated: true)
         }
+    }
+    
+    @objc func showScoreTapped() {
+        let ac = UIAlertController(title: "Current Score", message: "Your current score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: nil))
+        present(ac,animated: true)
     }
 }
 
